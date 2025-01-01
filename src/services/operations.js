759780d8d5cd76f106d30f77c3c6f832efd2b7b1
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-01-01 18:12:34 +0100

    created reducer

--- src/services/operations.js
+++ src/services/operations.js
@@ -6,7 +6,7 @@ const currencyOptions = {
 export const getTotal = (cart) => {
   return cart.total?.toLocaleString(undefined, currencyOptions)
 }
-//add
+/* //add
 export const add = (product, cart, setCart) => {
   let bool = false
   cart.cart.forEach(object => {
@@ -74,4 +74,68 @@ export const remove = (product, cart, setCart) => {
     }
   }
   )
-} 
+}  */
+
+export const add = (state, product) => {
+  let bool = false
+  state.cart.forEach(object => {
+    if (product.id === object.id) {
+      return bool = true;
+    }
+  })
+
+  if (bool) {
+    product.quantity ? product.quantity += 1 : product.quantity = 2
+      return {
+        cart: [...state.cart.filter(object => {
+          return object.id != product.id
+        }), product],
+        total: state.total + product.priceCents
+      }
+    bool = false;
+  } else {
+      return {
+        cart: [...state.cart.filter(object => {
+          return object.id != product.id
+        }), product] ,
+        total: state.total + product.priceCents
+      }
+    } 
+}
+
+export const remove = (state, product) => {
+  let outputState = {};
+  state.cart.forEach(purchasedProduct => {
+
+    if (purchasedProduct.id === product.id && purchasedProduct?.quantity > 0) {
+
+      purchasedProduct.quantity -= 1;
+      if (purchasedProduct.quantity === 0) {
+        outputState = {
+          cart: [...state.cart.filter(object => {
+            return object.id != product.id
+          })],
+          total: state.total > 0 ? state.total - product.priceCents : 0
+        }
+      } else {
+        outputState = {
+          cart: [...state.cart.filter(object => {
+            return object.id != product.id
+          }), purchasedProduct],
+          total: state.total > 0 ? state.total - product.priceCents : 0
+        }
+      }
+
+    } else if (purchasedProduct.id === product.id) {
+      outputState = {
+        cart: state.cart.filter(object =>  object != product),
+        total: state.total > 0 ? state.total - product.priceCents : 0
+      }
+    } else {
+      outputState = state;
+    }
+  } 
+  )
+
+  return outputState;
+}
