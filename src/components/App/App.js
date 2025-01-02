9a4d7c846108ddcc6f9a40d2d1e04063fca02e73
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-01-02 07:28:36 +0100

    used context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,18 @@
 import Product from '../Product/Product.js';
 import Header from '../Header/Header.js';
+import ProductsContext from '../ProductContext/ProductsContext.js';
+import { Products2 } from '../../data/products.js'
 
 function App() {
   return (
     <>
     <header>Git repository 👉🏻<a href="https://github.com/lorenzoVwebDev/E-commerce">E-commerce</a></header>
+    <ProductsContext.Provider>
     <Header/>
     <div className="App">
       <Product/>
     </div>
+    </ProductsContext.Provider>
     </>
   );
 }
